04fe5d0079e34b2e830849b202fcfdd89826e269
AdamCav <adamcaverhill@gmail.com>
2024-04-25 22:04:16 -0400

    Added color square, mono font

--- src/component/ArtTools.jsx
+++ src/component/ArtTools.jsx
@@ -10,9 +10,35 @@ function convertToColorP(r,g,b,string) {
     }
     return (
             <p style={style}>{string}</p>
-
     )
+}
 
+function generateColorButtons() {
+    const output = [];
+    let count = 0;
+    for (let r=0;r<6;r++){
+        for (let g=0;g<6;g++){
+
+            for (let b=0;b<6;b++){
+
+                let style;
+                if (r+g+b<3){
+                    continue
+                }
+
+                    style = {
+                        backgroundColor: 'rgb('+r*255/5+','+g*255/5+','+b*255/5+')',
+                        float: 'left',
+                        width: '50px',
+                        textAlign: 'center',
+                    }
+            
+                output.push(
+                    <div style={style}>{r}{g}{b}</div>)
+            }
+        }
+    }
+    return output;
 }
 
 function convertTextToColor(input){
@@ -27,10 +53,7 @@ function convertTextToColor(input){
         }
     })
     return output
-
 }
-
-
 
 function ArtTools(props) {
     const id = useId();
@@ -39,6 +62,7 @@ function ArtTools(props) {
     console.log(artOutput)
     return (
         <div style={{backgroundColor: 'black',
+        fontFamily: 'monospace',
         position: 'fixed',
         top: '0',
         bottom: '0',
@@ -46,12 +70,15 @@ function ArtTools(props) {
         right: '0',
         overflow: 'auto',
         color: 'rgb(155,155,155)'}}>
-        <div style={{display:'flex'}}>
-      <textarea style={{width:'60%', height:'300px', background:'black', color:'antiquewhite'}} id={id} value={art} onInput={e => setArt(e.target.value)}/>
-      <div>{art.length}</div>
+            <div style={{display:'flex', justifyContent: 'space-between'}}>
+      <textarea style={{width:'60%', minWidth:'60%', height:'300px', background:'black', color:'antiquewhite'}} id={id} value={art} onInput={e => setArt(e.target.value)}/>
+      <div style={{color:'black'}}>
+        {generateColorButtons().map(button=>button)}
+        </div>
       </div>
       <div/>
-            
+
+      <div>{art.length}</div>
       <p>Art Preview:</p>
       {artOutput.map(para => para)}
       </div>
